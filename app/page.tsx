f3d52daecce53f8d33df0a189c77b8e4bff2a5f5
SergioIgnatev <ignatevsergey17@gmail.com>
2023-11-05 23:15:32 +0300

    image

--- app/page.tsx
+++ app/page.tsx
@@ -5,24 +5,14 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Link href='/users'>Users</Link>
       <div className="btn btn-primary">btn-primary</div>
-      <div className="rating">
-  <input type="radio" name="rating-2" className="mask mask-star" />
-  <input type="radio" name="rating-1" className="mask mask-star"  />
-  <input type="radio" name="rating-1" className="mask mask-star" />
-  <input type="radio" name="rating-1" className="mask mask-star" checked/>
-  <input type="radio" name="rating-1" className="mask mask-star" />
-</div>
-<div className="rating">
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-</div>
-<div className="mockup-phone border-primary">
-  <div className="camera"></div> 
-  <div className="display">
-    <div className="artboard artboard-demo phone-1">Hi.</div>
+      <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col lg:flex-row">
+    <img src="/images/stock/photo-1635805737707-575885ab0820.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+    <div>
+      <h1 className="text-5xl font-bold">Box Office News!</h1>
+      <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+      <button className="btn btn-primary">Get Started</button>
+    </div>
   </div>
 </div>
     </main>
