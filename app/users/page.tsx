dd95e1e9a8162cfbe40de446451626f288dc912a
SergioIgnatev <ignatevsergey17@gmail.com>
2023-11-05 14:38:32 +0300

    component

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -7,13 +7,17 @@ interface Users{
         city:string
     }
 }
+function MappedUser({name,address,email}:Users){
+    return <div  className="p-6 border-blue-800 border-[2px] w-[300px] bg-blue-300">
+    {name}<br></br> {address.city} <div className="text-[19px]">{email}</div></div>
+}
 
 const  User= async()=>{
     const res= await fetch('https://jsonplaceholder.typicode.com/users')
     const users:Users[] = await res.json()
     return <div className="text-center ">
 <h1 className="text-center">USERS</h1>
-<h2 className="flex flex-wrap gap-[10px]">{users.map(user=><div key={user.id}>{user.name}</div>)}</h2>
+<h2 className="flex flex-wrap gap-[10px]">{users.map(user=><MappedUser key={user.id} {...user}/>)}</h2>
 </div>
 }
 export default User
