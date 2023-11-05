597e1da4ac367e7a7aea6e1a6bb07a1275221f98
SergioIgnatev <ignatevsergey17@gmail.com>
2023-11-05 14:02:50 +0300

    h

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Link href='/users'>Users</Link>
    <h1>HELLO MOSHE</h1>
-   
+
     </main>
   )
 }
