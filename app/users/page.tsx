47330d948ed1c8df6deff5d3cbb355647cc6f959
SergioIgnatev <ignatevsergey17@gmail.com>
2023-12-04 22:42:50 +0300

    ororo

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,14 +5,19 @@ import { Props } from "../page"
 export default async function Users({params}:{params:{id:number}}){
     let base:Props[]=await server()
     let i=base[params.id-1]
-    let mappedItems=base.map(m=><div key={m.id} className="text-black text-[20px] w-[300px] p-[20px]  m-[10px]">
-      <InView duration={0.2} > <Link  href={`/users/blog/${m.id}`}><div className="text-[40px]">{m.id}</div>
-        <div className="">{m.name}</div>
+    let mappedItems=base.map(m=>
+        <div className="group relative border-[1px] h-[300px] w-[250px]   owerflow-scroll " key={m.id}>
+            <div className="group-hover:opacity-100 rounded-lg text-white bg-slate-600 absolute w-[100%] h-[100%] opacity-0 p-2">{m.body}</div>
+    <div className="group-hover:opacity-10 w-[250px] opacity-100  m-[10px] relative group" ><InView duration={0.2} >
+    
+        <div  className="text-[20px]  p-2  ">
+       <Link  href={`/users/blog/${m.id}`}><div className="text-[40px] text-center">{m.id}</div>
+        <div className="text-[red]">{m.name}</div>
         </Link>
-        </InView>
-        </div>)
+        
+        </div></InView></div></div>)
 return <div className="bg">
     
-   <div className="flex flex-wrap">{mappedItems}</div>
+   <div className="flex flex-wrap gap-9">{mappedItems}</div>
     </div>
 }
