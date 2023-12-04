3e1b7d4618df6e737473f213075533122b774b77
SergioIgnatev <ignatevsergey17@gmail.com>
2023-12-04 17:31:04 +0300

    k

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -5,7 +5,7 @@ import { Props } from "../page"
 export default async function Users({params}:{params:{id:number}}){
     let base:Props[]=await server()
     let i=base[params.id-1]
-    let mappedItems=base.map(m=><div className="text-black text-[20px] w-[300px] p-[20px]  m-[10px]">
+    let mappedItems=base.map(m=><div key={m.id} className="text-black text-[20px] w-[300px] p-[20px]  m-[10px]">
       <InView duration={0.2} > <Link  href={`/users/blog/${m.id}`}><div className="text-[40px]">{m.id}</div>
         <div className="">{m.name}</div>
         </Link>
